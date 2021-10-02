ecb8e8266e68774fe021a2884fbffbacc9785bf6
Chok <8801373+cmyip@users.noreply.github.com>
2021-10-03 02:40:04 +0800

    move ormconfig for heroku deploy

---
+++ ormconfig.js
@@ -0,0 +1,27 @@
+module.exports = {
+    type: "postgres",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "scout",
+    database: process.env.DB_NAME || "test_db",
+    charset: "utf8",
+    driver: "postgres",
+    ssl: !!process.env.DB_SSL,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+    synchronize: process.env.NODE_ENV !== "production",
+    entities:[
+        "**/**.entity.ts"
+    ],
+    logging: "error",
+    migrations: ["migration/*.ts"],
+    cli: {
+        migrationsDir: "migration"
+    },
+    connectTimeout: 30000,
+    acquireTimeout: 30000
+};
